f457c0067b2d501250f7174da99d2f48f6e061e6
aniketbadole <aniket.b@outlook.com>
2021-08-29 15:44:54 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,17 +10,15 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading..";
   messageTwo.textContent = "";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageTwo.textContent = data.error;
-          messageOne.textContent = "";
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageTwo.textContent = data.error;
+        messageOne.textContent = "";
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const mapbox = require("./utils/mapbox");
 const weatherstack = require("./utils/weatherstack");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, "../public");
@@ -88,6 +89,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
